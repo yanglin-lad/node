7372d6567c18db0351b0a304db742f35e223bbee
lucky-Octocat <17615978618@163.com>
2023-01-06 21:25:13 +0800

    fs模块的readFile方法

--- fs/readFile.js
+++ fs/readFile.js
@@ -1,5 +1,8 @@
 const fs = require('fs')
 // fs 是 node 的文件系统模块，通过此模块的 readFile方法，可以读取文件
+
+// err 是错误
+// data 是数据，buffer数据类型的 一个二进制类型，需要 toString()
 fs.readFile('hello.txt', (err, data) => {
   if (err) {
     console.log(err, 'err')
