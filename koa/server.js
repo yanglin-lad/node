c3550fc5d8003bd4a95c684021a421ed6f947edf
lucky-Octocat <17615978618@163.com>
2023-01-07 15:40:19 +0800

    koa框架，开发web服务器

---
+++ koa/server.js
@@ -0,0 +1,16 @@
+const Koa = require('koa') // 引入 koa 构造函数，来创建koa应用
+// 引入的 koa-router 是一个函数，需要再调用一次
+const router = require('koa-router')()
+const app = new Koa() // 创建应用
+
+// use 方法，koa 框架的方法，可以引入一个中间件
+// 中间件 在 请求和响应之间执行
+app.use(async (ctx) => {
+  // ctx 上下文
+  ctx.body = 'hello Koa'
+})
+
+// 设置监听端口
+app.listen(8618, () => {
+  console.log('Server is running!!!')
+})
